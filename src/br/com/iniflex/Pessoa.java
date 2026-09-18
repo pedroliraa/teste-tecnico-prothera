233e1aa4901ffad0780648c5bebc5f709e93af49
pedroliraa <pedrolirabandeira@gmail.com>
2026-09-18 16:48:56 -0300

    feat: create Pessoa class

---
+++ src/br/com/iniflex/Pessoa.java
@@ -0,0 +1,24 @@
+package br.com.iniflex;
+
+import java.time.LocalDate;
+
+public class Pessoa {
+	
+	private String nome;
+	private LocalDate dataNascimento;
+	
+	public Pessoa(String nome, LocalDate dataNascimento) {
+		super();
+		this.nome = nome;
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+}
